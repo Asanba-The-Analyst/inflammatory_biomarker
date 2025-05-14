fbbea789f0f7afc903e414e622c1562b6538b33c
Asanba Alexander <cefes.cs@gmail.com>
2025-05-14 10:29:47 +0000

    added variables names in scrit to be used

---
+++ scripts/variables_list.R
@@ -0,0 +1,129 @@
+my_list <- list(
+  "cbc_lbtstdat",
+  "cbc_lbperf_1",
+  "cbc_hb_lborres",
+  "cbc_lbperf_2",
+  "cbc_hct_lborres",
+  "cbc_lbperf_3",
+  "cbc_wbc_lborres",
+  "cbc_lbperf_4",
+  "cbc_neu_pct_lborres",
+  "cbc_lbperf_5",
+  "cbc_neu_fcc_lborres",
+  "cbc_lbperf_6",
+  "cbc_lymph_pct_lborres",
+  "cbc_lbperf_7",
+  "cbc_lymph_fcc_lborres",
+  "cbc_lbperf_8",
+  "cbc_eryth_mm_lborres",
+  "cbc_lbperf_9",
+  "cbc_mcv_lborres",
+  "cbc_lbperf_10",
+  "cbc_mch_lborres",
+  "cbc_lbperf_11",
+  "cbc_mchc_gdl_lborres",
+  "cbc_lbperf_12",
+  "cbc_plate_lborres",
+  "cbc_lbperf_13",
+  "cbc_mono_pct_lborres",
+  "cbc_lbperf_14",
+  "cbc_mono_fcc_lborres",
+  "cbc_lbperf_15",
+  "cbc_eos_pct_lborres",
+  "cbc_lbperf_16",
+  "cbc_eos_fcc_lborres",
+  "cbc_lbperf_17",
+  "cbc_rdw_pct_lborres",
+  "cbc_lbperf_18",
+  "cbc_pdw_ct_lborres",
+  "cbc_lbperf_19",
+  "cbc_pct_pct_lborres"
+)
+my_list <- list(
+  "lft_lbperf_4",
+  "tbilirubin_lborres",
+  "lft_lbperf_5",
+  "dbilirubin_lborres",
+  "lft_lbperf_6",
+  "tprotein_lborres",
+  "lft_lbperf_7",
+  "albumin_lborres",
+  "lft_lbperf_8",
+  "gammagt_lborres",
+  "lft_lbperf_9",
+  "ibilirubin_lborres"
+)
+
+
+my_list <- list(
+  "bgluc_lbtstdat",
+  "bgluc_lbperf_1",
+  "bgluc_pretest_mmoll_lborres",
+  "bgluc_lbperf_2",
+  "bgluc_oral_1hr_mmoll_lborres",
+  "bgluc_lbperf_3",
+  "bgluc_oral_2hr_mmoll_lborres",
+  "hba1c_lbtstdat",
+  "hba1c_test_yn",
+  "hba1c_lborres",
+  "hba1c_prcnt",
+  "tb_backup_lborres"
+)
+
+my_list <- list(
+  "bgluc_lbtstdat",
+  "bgluc_lbperf_1",
+  "bgluc_pretest_mmoll_lborres",
+  "bgluc_lbperf_2",
+  "bgluc_oral_1hr_mmoll_lborres",
+  "bgluc_lbperf_3",
+  "bgluc_oral_2hr_mmoll_lborres",
+  "hba1c_lbtstdat",
+  "hba1c_test_yn",
+  "hba1c_lborres",
+  "hba1c_prcnt"
+)
+
+
+my_list <- list(
+  "rbc_lbperf_3",
+  "lborres",
+  "rbc_g6pd_lbstdat",
+  "ua_lbtstdat",
+  "ua_dipstick",
+  "ua_lbperf_1",
+  "ua_prot_lborres",
+  "ua_lbperf_2",
+  "ua_leuk_lborres",
+  "ua_lbperf_3",
+  "ua_nitrite_lborres",
+  "tb_lbtstdat",
+  "tb_lbperf_1",
+  "tb_cnfrm_lborres",
+  "tb_lbperf_2"
+)
+
+
+#hepatitis
+my_list <- list(
+  "hev_lbtstdat",
+  "hev_lbperf_1",
+  "hev_igm_lborres",
+  "hev_lbperf_2",
+  "hev_igg_lborres"
+)
+
+
+gest_diab_list <- list(
+  "gest_diab_mhoccur",
+  "gest_diab_srce_1",
+  "gest_diab_srce_2",
+  "gest_diab_srce_3",
+  "gest_diab_proccur_1",
+  "gest_diab_proccur_2",
+  "gest_diab_proccur_3",
+  "gest_diab_proccur_4",
+  "gest_diab_proccur_88",
+  "gest_diab_proccur_77",
+  "gest_diab_proccur_99"
+)
